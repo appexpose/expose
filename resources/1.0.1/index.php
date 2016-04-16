74b86236008ca5a06243b1863d2b908399a9c653
MiGatoSeneca <pablo@citious.com>
2016-04-16 12:55:05 +0200

    update

--- resources/1.0.1/index.php
+++ resources/1.0.1/index.php
@@ -1,13 +1,14 @@
 <?php
-echo "Files:";
-$path = ".";
+echo "<h1>Index of Resources</h1>";
+$path = "<ul>";
 $dh = opendir($path);
 $i=1;
 while (($file = readdir($dh)) !== false) {
     if($file != "." && $file != ".." && $file != "index.php" && $file != ".htaccess" && $file != "error_log" && $file != "cgi-bin") {
-        echo "<a href='$path/$file'>$file</a><br /><br />";
+        echo "<li><a href='$path/$file'>$file</a></li>";
         $i++;
     }
 }
 closedir($dh);
+$path = "</ul>";
 ?>
