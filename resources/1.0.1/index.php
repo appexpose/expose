166aedf703915e9d8aabc64614f58defdc96896b
MiGatoSeneca <pablo@citious.com>
2016-04-18 09:56:27 +0200

    update

--- resources/1.0.1/index.php
+++ resources/1.0.1/index.php
@@ -2,6 +2,8 @@
 echo "<h1>Index of Expose Resources</h1>";
 $path = ".";
 echo "<ul>";
+echo "<li><a href='$path/.'>.</a></li>";
+echo "<li><a href='$path/..'>..</a></li>";
 $dh = opendir($path);
 $i=1;
 while (($file = readdir($dh)) !== false) {
