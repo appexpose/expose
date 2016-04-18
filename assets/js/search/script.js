2b9590b70a3adea6db40902d441bfa317c836bae
MiGatoSeneca <pablo@citious.com>
2016-04-18 14:30:50 +0200

    update

--- assets/js/search/script.js
+++ assets/js/search/script.js
@@ -233,7 +233,7 @@ $("#form-search").validate({
     $("#form-search-display>.visible-loading").removeClass("hidden");
     $_number=$("#form-search #number").val();
     $_number=$_number.replace("+","00");
-    $_number=$_number.replace("/ /g","");
+    $_number=$_number.str.split(' ').join('');
     localStorage.number=$_number;
     console.error(localStorage.number);
     list_comments(0,10);
